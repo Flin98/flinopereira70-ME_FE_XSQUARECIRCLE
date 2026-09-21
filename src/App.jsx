f48afb37aec4991fd3eff05e117854e03a93354e
Flino Pereira <flinopereira70@gmail.com>
2026-09-21 14:15:42 +0530

    feat: implement square and circle component layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="page-wrapper">
+      <div className="content-container">
+        <h1 className="title">Sqaure & Circle</h1>
+        <div className="square">
+          <div className="circle"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
